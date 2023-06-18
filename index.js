1b4f6da254d15313e9d5f4714c692e6acef621bb
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-19 01:41:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names, surprise){
+    let newArr = []
+    names.forEach(element =>{
+        newArr.push(`Thank you, ${element}, for the wonderful ${surprise} gift!`)
+
+    });
+    return newArr
+}
+
+
