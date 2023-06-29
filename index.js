06b039c3061c86f2813bfe40b1f2c973f5f82e64
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-29 21:53:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-// Code your solutions in this file
-function writeCards(names, surprise){
-    let newArr = []
-    names.forEach(element =>{
-        newArr.push(`Thank you, ${element}, for the wonderful ${surprise} gift!`)
+//Code your solutions in this file
 
-    });
-    return newArr
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
 }
+
+
+const names = ["Charlie", "Samip", "Ali"];
+const eventName = "birthday";
+
+const thankYouMessages = writeCards(names, eventName);
+console.log(thankYouMessages);
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  
+  
+  
+    
+
 
 
